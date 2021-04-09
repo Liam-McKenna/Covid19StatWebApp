d618218bca9bad0061a2322423a746d77e69ef79
Liam-McKenna <liam.zendesign@gmail.com>
2021-04-10 00:47:56 +0100

    1.3 - API, database, searchbar

--- components/searchbar.php
+++ components/searchbar.php
@@ -1,11 +1,34 @@
-<?php
-//print_r($_POST);
-if (isset($_POST['submit'])) {
-    echo "Search is: " . $_POST['search'];
-}
-?>
+<!-- <input type="text" name="searchBar" id="searchBar" placeholder="search for country" />
+
+<script>
+    const searchbar = document.getElementById('searchBar');
+
+    searchbar.addEventListener('keyup', (e) => {
+        console.log(e.target.value);
+
+    });
+</script> -->
+
 
 <form action="" method="POST">
-    Name: <input type="text" name="search" value="">
-    <input type="submit" name="submit" valuer="submit">
+    <p>Enter Country Name: </p>
+    <input type="text" name="search" value="">
+    <input type="submit" name="SearchSubmit" value="Search">
 </form>
+
+<?php
+
+
+if (isset($_POST['SearchSubmit'])) {
+    $country =  $_POST['search'];
+    $_POST = array();
+
+    header()
+
+    // echo "
+    // <script>
+    // parent.self.location = \"singleCountry.php?countryCode=ie&countryName=" . $country . "\";
+    // </script>
+    // ";
+}
+?>
